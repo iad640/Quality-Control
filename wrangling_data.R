ac1ac4c9a6e756f2c0d53168a550a1ed9f32da42
iad640 <ivan.alonso@uib.es>
2021-11-23 16:50:33 +0100

    change of name

--- wrangling_data.R
+++ wrangling_data.R
@@ -1,3 +1,2 @@
 #This script is just to see if git bahs still syncs the project after changing its location in the local repositary, i.e., after changing the project directory path in the local PC
 x<-2
-
